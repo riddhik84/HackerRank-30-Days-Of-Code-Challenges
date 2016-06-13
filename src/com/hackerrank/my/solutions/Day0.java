53814b64d46d86dd76e52963f2f6e009f99b9a0c
riddhik84 <riddhik84@gmail.com>
2016-06-13 13:24:03 +0530

    Day0 Solution

--- src/com/hackerrank/my/solutions/Day0.java
+++ src/com/hackerrank/my/solutions/Day0.java
@@ -1,9 +1,19 @@
 package com.hackerrank.my.solutions;
 
-public class Day0 {
-	
-	public static void main(String args[]){
-		
-	}
+import java.util.Scanner;
 
+public class Day0 {
+
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in); // use the Scanner class to read from stdin
+		//Sample input: Welcome to 30 Days of Code!
+		String inputString = scan.nextLine(); // read a line of input and save it to a variable
+		scan.close(); // close the scanner
+
+		// Your first line of output goes here
+		System.out.println("Hello, World.");
+
+		// Write the second line of output
+		System.out.println(inputString);
+	}
 }
