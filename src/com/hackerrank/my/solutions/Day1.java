9dd9e78701f523116a79325778ee47a92e62b103
riddhik84 <riddhik84@gmail.com>
2016-06-15 12:26:59 +0530

    Day1 Solution

--- src/com/hackerrank/my/solutions/Day1.java
+++ src/com/hackerrank/my/solutions/Day1.java
@@ -17,7 +17,7 @@ public class Day1 {
 		ii = scan.nextInt();
 		dd = scan.nextDouble();
 		ss = scan.next();
-		ss = ss + scan.nextLine();
+		ss = ss + scan.nextLine(); //Need to scan one more time to get full line as input
 
 		System.out.println(i + ii);
 		System.out.println(d + dd);
