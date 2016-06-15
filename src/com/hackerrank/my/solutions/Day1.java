71356b7e2ebcbb98cfef7b0e561735dbb2e47b57
riddhik84 <riddhik84@gmail.com>
2016-06-15 12:24:12 +0530

    Day1 Solution

---
+++ src/com/hackerrank/my/solutions/Day1.java
@@ -0,0 +1,28 @@
+package com.hackerrank.my.solutions;
+
+import java.util.Scanner;
+
+public class Day1 {
+
+	public static void main(String... args){
+		int i = 4;
+		double d = 4.0;
+		String s = "HackerRank ";
+
+		Scanner scan = new Scanner(System.in);
+		int ii = 0;
+		double dd = 0.0;
+		String ss = null;
+
+		ii = scan.nextInt();
+		dd = scan.nextDouble();
+		ss = scan.next();
+		ss = ss + scan.nextLine();
+
+		System.out.println(i + ii);
+		System.out.println(d + dd);
+		System.out.println(s + ss);
+		
+		scan.close();
+	}
+}
