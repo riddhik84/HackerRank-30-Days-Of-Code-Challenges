59ff9ad85334836a5316ea0db9ed6ba1c14b2325
riddhik84 <riddhik84@gmail.com>
2016-06-16 19:17:29 +0530

    Day3 Solution

---
+++ src/com/hackerrank/my/solutions/Day3.java
@@ -0,0 +1,31 @@
+package com.hackerrank.my.solutions;
+
+import java.util.Scanner;
+
+public class Day3 {
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		String str = "";
+
+		//If remainder is 1, the number is Odd
+		if(n%2 == 1){
+			//Odd
+			str = "Weird";
+		}else{
+			//Even
+			if(n>=2 && n<=5){
+				str = "Not Weird";
+			} else if (n>=6 && n<=20){
+				str = "Weird";
+			} else if(n > 20) {
+				str = "Not Weird";
+			}
+		}
+
+		System.out.println(str);
+	}
+
+}
