8e7555a8657b62d5ad56576d4d7624285aafcd27
riddhik84 <riddhik84@gmail.com>
2016-06-20 12:47:20 +0530

    Day5 Solution

---
+++ src/com/hackerrank/my/solutions/Day5.java
@@ -0,0 +1,16 @@
+package com.hackerrank.my.solutions;
+
+import java.util.Scanner;
+
+public class Day5 {
+	public static void main(String[] args){
+		 Scanner in = new Scanner(System.in);
+	        int N = in.nextInt();
+	        
+	        for(int i = 1; i <=10; i++){
+	        	int mul = i * N;
+	        	System.out.println(N + " x " + i + " = " + mul);
+	        }
+	        in.close();
+	}
+}
