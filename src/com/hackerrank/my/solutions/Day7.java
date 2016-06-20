3a1a28a0f40b2f04730ab9961cd5d618b502bdbe
riddhik84 <riddhik84@gmail.com>
2016-06-20 13:16:26 +0530

    Day7 Solution

---
+++ src/com/hackerrank/my/solutions/Day7.java
@@ -0,0 +1,19 @@
+package com.hackerrank.my.solutions;
+
+import java.util.Scanner;
+
+public class Day7 {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        int len = arr.length;
+        for(int j = len - 1; j >= 0; j--){
+        	System.out.print(arr[j] + " ");
+        }
+        in.close();
+	}
+}
