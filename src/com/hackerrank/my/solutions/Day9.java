69f340d0f5b5dd05893b3f5c8ab38ffda114fa15
riddhik84 <riddhik84@gmail.com>
2016-06-23 13:03:14 +0530

    Day9 Solution

---
+++ src/com/hackerrank/my/solutions/Day9.java
@@ -0,0 +1,21 @@
+package com.hackerrank.my.solutions;
+
+import java.util.Scanner;
+
+public class Day9 {
+	public static void main(String args[]){
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		
+		int fact = factorial(num);
+		System.out.println(fact);
+	}
+
+	static int factorial(int num){
+		if(num > 1){
+			return num * factorial(num - 1);
+		} else {
+			return num;
+		}
+	}
+}
