d5d2b98c4b7c153def4b54f746e9c224b1b253cd
riddhik84 <riddhik84@gmail.com>
2016-07-13 11:20:08 +0530

    Day11 Solution

--- src/com/hackerrank/my/solutions/Day11.java
+++ src/com/hackerrank/my/solutions/Day11.java
@@ -1,22 +1,32 @@
 package com.hackerrank.my.solutions;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+/*
+ * Solution referenced from discussion help topics
+ */
 
 public class Day11 {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-        int arr[][] = new int[6][6];
-        for(int i=0; i < 6; i++){
-            for(int j=0; j < 6; j++){
-                arr[i][j] = in.nextInt();
-            }
-        }
-        ArrayList<Integer> hourGlassSum = new ArrayList<>();
-        int sum = 0;
-        for(int i = 0, j = 0; j < 6 ; i++,j++){
-        	sum = arr[i][j] + arr[i][j+1] + arr[i][j+2];
-        }
-             
-      }
+		int arr[][] = new int[6][6];
+		for(int i=0; i < 6; i++){
+			for(int j=0; j < 6; j++){
+				arr[i][j] = in.nextInt();
+			}
+		}
+		int hourGlassSum[] = new int[16];
+		int hourGlass = 0;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				hourGlassSum[hourGlass] = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+						+ arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
+								+ arr[i+2][j+2];
+				hourGlass++;
+			}
+		}
+		Arrays.sort(hourGlassSum);
+		System.out.println(hourGlassSum[15]);
+	}
 }
